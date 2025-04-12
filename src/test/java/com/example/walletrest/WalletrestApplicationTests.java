fbb87b5a8de0da18d2f437dee203c4d43e9939d6
Abdulrasaq Durosomo <durosomo.abdulrasaq@gmail.com>
2025-04-12 04:13:43 +0100

    initial setup

---
+++ src/test/java/com/example/walletrest/WalletrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
